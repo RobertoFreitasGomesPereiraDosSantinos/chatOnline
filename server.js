20fc68420974c03f78cf5fbd56e29492ac734f48
RobertoFreitasGomesPereiraDosSantinos <99353140+RobertoFreitasGomesPereiraDosSantinos@users.noreply.github.com>
2022-12-02 14:29:46 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const fs = require("fs");
 const app = express();
 const http = require("http").Server(app);
 const serv_sockt = require("socket.io")(http);
-const porta = process.env.PORT || 3000;
-
+const porta = process.env.PORT || 3300;
+//------------------------------------
 let historico = [];
 let usuarios = [];
+let ID;
+let NOME;
 
 app.use(express.static("publico"));
 
@@ -15,6 +17,8 @@ http.listen(porta, function () {
 });
 
 app.get("/", function (req, resp) {
+  ID = req.query.id;
+  NOME = req.query.nome;
   fs.readFile(__dirname + "/chat.html", function (err, data) {
     if (err) {
       return resp.end("falha ao carregar arquivo");
@@ -24,41 +28,35 @@ app.get("/", function (req, resp) {
 });
 
 serv_sockt.on("connection", function (socket) {
-  socket.on("login", function (apelido, callback) {
-    if (!(apelido.nick in usuarios)) {
-      socket.nickname = apelido.nick;
-      socket.id = apelido.id;
-      usuarios[apelido.nick] = {sckt: socket, id: apelido.id};
-      /*
-      da pra usar um forEach pra percorrer a array usuarios
-      e usar o id do objeto, pra saber quais pessoas estão
-      num projeto, e da um emit todos os users com id´s iguais
-      assim sempre será menssagens privadas
-      */
-      // historico.forEach(function (dados) {
-      //   socket.emit("chat", dados);
-      // });
-      const msg_form = {
-        msg: `[${pegarDataAtual()}]${apelido.nick} acabou de entrar na sala`,
-        tipo: "sistema",
-      };
-      _saveMsg_(msg_form);
-      serv_sockt.emit("atualizar usuarios", Object.keys(usuarios));
-      for (const key in usuarios) {
-        if (usuarios[key].id == socket.id) {
-          usuarios[key].sckt.emit('chat', msg_form);
-        }
-      }
-      callback(true);
-    } else {
-      callback(false);
+  usuarios[NOME] = {sckt: socket, id: ID}
+  socket.id = ID;
+  socket.nickname = NOME;
+  socket.emit('login', NOME);
+  const msg_form = {
+    msg: `[${pegarDataAtual()}]${NOME} está online`,
+    id: socket.id,
+    tipo: "sistema",
+  };
+  _saveMsg_(msg_form);
+  for (const key in usuarios) {
+    if (usuarios[key].id == socket.id) {
+      usuarios[key].sckt.emit('chat', msg_form);
     }
+  }
+
+  socket.on('historico', function(nom) {
+    historico.forEach(function(valor) {
+      if (valor.id == socket.id) {
+        usuarios[nom].sckt.emit('chat', valor);
+      }
+    });
   });
 
   socket.on("chat", function (msg) {
     msg = `[${socket.nickname} ${pegarDataAtual()}]: ${msg}`;
       const msg_form = {
         msg: msg,
+        id: socket.id,
         tipo: ''
       };
       _saveMsg_(msg_form);
@@ -71,29 +69,29 @@ serv_sockt.on("connection", function (socket) {
 
   socket.on("disconnect", function () {
     delete usuarios[socket.nickname];
+    NOME = null;
+    ID = null;
     const msg_form = {
-      msg: `[${pegarDataAtual()}]${socket.nickname} saiu da sala`,
+      msg: `[${pegarDataAtual()}]${socket.nickname} está offline`,
+      id: socket.id,
       tipo: "sistema",
     };
     _saveMsg_(msg_form);
-    serv_sockt.emit("atualizar usuarios", Object.keys(usuarios));
     for (const key in usuarios) {
       if (usuarios[key].id == socket.id) {
         usuarios[key].sckt.emit('chat', msg_form);
       }
     }
   });
-
-  function pegarDataAtual() {
-    var dataAtual = new Date();
-    var hora = (dataAtual.getHours() < 10 ? "0" : "") + dataAtual.getHours();
-    var minuto =
-      (dataAtual.getMinutes() < 10 ? "0" : "") + dataAtual.getMinutes();
-    var dataFormatada = hora + ":" + minuto;
-    return dataFormatada;
-  }
-
-  _saveMsg_ = (neo_msg) => {
-    historico.push(neo_msg);
-  };
 });
+
+function pegarDataAtual() {
+  var dataAtual = new Date();
+  var hora = (dataAtual.getHours() < 10 ? "0" : "") + dataAtual.getHours();
+  var minuto =
+    (dataAtual.getMinutes() < 10 ? "0" : "") + dataAtual.getMinutes();
+  var dataFormatada = hora + ":" + minuto;
+  return dataFormatada;
+}
+
+_saveMsg_ = (neo_msg) => {historico.push(neo_msg);};
